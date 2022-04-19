f862410b7b29757d00db150a30f9e104386c1bf4
Uma Negi <89074181+umanegi8@users.noreply.github.com>
2022-04-19 18:44:56 +0530

    Basics of R
    In this I have started with R and its basics: Functions, Data Types, Variable, comments, Vector and Pipes.

---
+++ Starting with R.R
@@ -0,0 +1,31 @@
+#Foundation of R
+
+#Functions, Comments, Variables, Data Types, Vectors, Pipes
+
+print("Starting with R")                         #function
+
+#This helps in knowing about the function
+?print                                    
+
+#defining variable
+first_variable <- "This is my  first variable"    #string
+second_variable <- 46                             #numeric
+
+
+#creating vector
+vec_01 <- c(13, 46, 7, 60, 20)
+vec_02 <- c(2, 3, 1, 1, 1)
+
+#arthimetic vector operaion
+add <- vec_01 + vec_02
+sub <- vec_01 - vec_02
+mul <- vec_01 * vec_02
+div <- vec_01 / vec_02
+
+add
+sub
+mul
+div
+
+#creating Pipe
+
